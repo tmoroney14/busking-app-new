93efdfb90822df39ca2f5f94e94c65962bb64648
tmoroney14 <thomas.moroney1010@gmail.com>
2019-10-05 23:59:22 +0100

    1 file modified, 1 added

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { CommonModule } from '@angular/common';
+
+@NgModule({
+  imports: [
+    CommonModule
+  ],
+  declarations: []
+})
+export class AppRoutingModule { }
