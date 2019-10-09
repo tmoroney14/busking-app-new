d2a67d755c2eb75ef59c753a5c86010c2209057e
tmoroney14 <thomas.moroney1010@gmail.com>
2019-10-09 19:00:34 +0100

    6 files modified, 2 added, 1 deleted

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,14 +2,15 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 import { RouterModule } from '@angular/router';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppRoutingModule } from './app-routing.module'
 import { AppComponent } from './app.component';
-import { HelloComponent } from './hello.component';
+import { ToolbarComponent } from './toolbar/toolbar.component';
 
 @NgModule({
   imports:      [ BrowserModule, FormsModule, RouterModule ],
-  declarations: [ AppComponent, HelloComponent ],
+  declarations: [ AppComponent, ToolbarComponent ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
