9c488b4a4ae98e511c47b2d73c89f3ff86615ea3
tmoroney14 <thomas.moroney1010@gmail.com>
2019-10-09 19:16:37 +0100

    2 files modified, 4 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 import { RouterModule } from '@angular/router';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import 'hammerjs';
 
-import { AppRoutingModule } from './app-routing.module'
+import { AppMaterialModule } from './app-material.module';
+import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
 import { ToolbarComponent } from './toolbar/toolbar.component';
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule, RouterModule ],
-  declarations: [ AppComponent, ToolbarComponent ],
+  imports:      [ BrowserModule, FormsModule, RouterModule, AppRoutingModule, AppMaterialModule ],
+  declarations: [ AppComponent, HomeComponent, ToolbarComponent ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
