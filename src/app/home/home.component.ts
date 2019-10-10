8cf63296f1f0d59c70e57eb03fdae268304f28e7
tmoroney14 <thomas.moroney1010@gmail.com>
2019-10-10 18:39:51 +0100

    4 files modified

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -6,7 +6,7 @@ import { BehaviorSubject, Observable } from 'rxjs';
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent {
+export class HomeComponent implements AfterViewInit {
 
   showToggle: string;
   mode: string;
@@ -38,5 +38,20 @@ export class HomeComponent {
   getScreenWidth(): Observable<number> {
     return this.screenWidth$.asObservable();
   }
+
+  ngAfterViewInit () {
+            !function(d,s,id){
+                var js: any,
+                    fjs=d.getElementsByTagName(s)[0],
+                    p='https';
+                if(!d.getElementById(id)){
+                    js=d.createElement(s);
+                    js.id=id;
+                    js.src=p+"://platform.twitter.com/widgets.js";
+                    fjs.parentNode.insertBefore(js,fjs);
+                }
+            }
+            (document,"script","twitter-wjs");
+    }
 
 }
