482b0565cd0f880d1e7f08c869ebfaa1e18bec59
tmoroney14 <thomas.moroney1010@gmail.com>
2019-10-10 18:29:51 +0100

    2 files modified, 1 added

---
+++ src/app/buskers.ts
@@ -0,0 +1,131 @@
+export const buskers = [
+  { 
+    name: 'Allie Sherlock',
+
+    image: 'https://static1.squarespace.com/static/5bb22f5711f7844a36489bca/5c5a0a06c83025d7e54d5388/5c5a0a4deef1a12d338b688d/1549404749200/?format=500w',
+    other: 'Popular Uploads',
+    videos: 'https://www.youtube.com/embed/videoseries?list=PLkNQ3IPg_TnUYqGHwWVdEq50pNR16T9iH',
+    live: 'https://www.youtube.com/embed/videoseries?list=PUGnsRwFS8Xm13lMAeouzT9g',
+    musict1: 'Original Music',
+    musict2: 'Spotify',
+    music1: 'https://www.youtube.com/embed/videoseries?list=PLkNQ3IPg_TnUg2aRGvD8Pz4K8UPekND7Y',
+    music2: 'https://open.spotify.com/embed/artist/2yTmZ1PwFKkO0FjQp2flPl',
+
+    description: 'Aged 14, Allie comes down from Cork with her dad every weekend to busk on Grafton Street.',
+    para1: 'Aged 14, Allie comes down from Cork with her dad every weekend to busk on Grafton Street.',
+    para2: '',
+    twitter: 'https://twitter.com/alliesherlock',
+    insta: 'https://www.instagram.com/alliesherlock',
+    facebook: 'https://www.facebook.com/alliesherlock2005',
+    youtube: 'https://www.youtube.com/channel/UCGnsRwFS8Xm13lMAeouzT9g',
+  },
+
+  { 
+    name: 'Padraig Cahill',
+
+    image: 'https://i.ytimg.com/vi/AregRiqHzbs/maxresdefault.jpg',
+    other: 'Official Channel Uploads',
+    videos: 'https://www.youtube.com/embed/videoseries?list=PL15laV1Ww91xMbNii_EW6ahejKZidr9vp',
+    live: 'https://www.youtube.com/embed/videoseries?list=UUQcfc9A8QgHGo4hjWx7DFjA',
+    musict1: 'Original Music',
+    musict2: 'Performance with Glen Hansard',
+    music1: 'https://www.youtube.com/embed/wUEtpfJUs7Q',
+    music2: 'https://www.youtube.com/embed/akaxQxEFrCo',
+
+    description: 'Padraig is aged 16 and from Wicklow. He regularly takes the bus to Dublin to busk on Grafton Street, drawing large crowds.',
+    para1: 'Padraig is aged 16 and from Wicklow. He regularly takes the bus to Dublin to busk on Grafton Street, drawing large crowds.',
+    para2: '',
+    twitter: '',
+    insta: 'https://www.instagram.com/padraigcahill_music',
+    facebook: 'https://www.facebook.com/padraigcahill.music',
+    youtube: 'https://www.youtube.com/channel/UCQcfc9A8QgHGo4hjWx7DFjA',
+  },
+
+  {
+    name: 'Jacob Coopman',
+
+    image: 'https://i.ytimg.com/vi/Zn-Y3nlBzSk/maxresdefault.jpg',
+    other: 'Performance',
+    videos: 'https://www.youtube.com/embed/videoseries?list=PL7Hzq1i4Oc9CzAs4sbMegFQZDQuxtIras',
+    live: 'https://www.youtube.com/embed/A8oG7m5Tr2M',
+    musict1: 'Original Music',
+    musict2: 'Jacob Coopman Essentials',
+    music1: 'https://open.spotify.com/embed/artist/54NoJVgezmrMKZp2G1wznx',
+    music2: 'https://www.youtube.com/embed/videoseries?list=PL7oB9Ax76_-Tr9IrDexRYn3F1nDMGU7_4',
+
+    description: 'Jacob is a Singer/Songwriter that can be found playing venues around Ireland and busking on Grafton Street playing a mixture of original and cover music.',
+    para1: 'Jacob’s Bali roots provides him with an indie pop style but his early influences such as Dave Grohl and Chris Cornell give an edge to his island vibe. He is a Singer/Songwriter who started playing drums at 9 years old. Self taught he also plays the guitar and bass.',
+    para2: 'Playing a mixture of original and cover music Jacob can be found playing venues around Ireland along with paying his dues by busking.',
+    twitter: '',
+    insta: 'https://www.instagram.com/jakumaru',
+    facebook: 'https://www.facebook.com/Jacobpkoopman',
+    youtube: 'https://www.youtube.com/user/MrJackPhantom',
+  },
+
+  {
+    name: 'Saibh Skelly',
+
+    image: 'https://i.ytimg.com/vi/U1a_KiDId0k/maxresdefault.jpg',
+    videos: 'https://www.youtube.com/embed/videoseries?list=PL7Hzq1i4Oc9Cvb7bXO4MXxN8YTIMLnPfs',
+    other: 'Interview',
+    live: 'https://www.youtube.com/embed/OQlZT4EVGO8',
+    musict1: 'Channel Uploads',
+    musict2: 'Collaboration',
+    music1: 'https://www.youtube.com/embed/videoseries?list=UUZfseNRa3zHbw2dD5JLiRLg',
+    music2: 'https://www.youtube.com/embed/Pxm30ayyCoA',
+
+    description: 'Aged 15, Saibh is from Dublin and busks on Grafton Street regularly, playing covers.',
+    para1: 'Aged 15, Saibh is from Dublin and busks on Grafton Street regularly, playing covers.',
+    para2: '',
+    twitter: '',
+    insta: 'https://www.instagram.com/saibhskellymusic',
+    facebook: 'https://www.facebook.com/saibhskellymusic',
+    youtube: 'https://www.youtube.com/channel/UCZfseNRa3zHbw2dD5JLiRLg',
+  },
+
+  {
+    name: 'Paul Jenkinson',
+
+    header: 'https://i.scdn.co/image/633d954057796269bc82bf591ce980ea450abdec',
+    image: 'https://i.ytimg.com/vi/vrVJ61cXXnY/maxresdefault.jpg',
+    other: 'Channel Uploads',
+    videos: 'https://www.youtube.com/embed/videoseries?list=PL7Hzq1i4Oc9CptZZxT71lwvSNhxTff0Ub',
+    live: 'https://www.youtube.com/embed/videoseries?list=UU1XkRRVbQSS8-3X48iMGOVA',
+    musict1: 'Original Music',
+    musict2: 'Spotify',
+    music1: 'https://open.spotify.com/embed/artist/54NoJVgezmrMKZp2G1wznx',
+    music2: 'https://www.youtube.com/embed/videoseries?list=PL7oB9Ax76_-Tr9IrDexRYn3F1nDMGU7_4',
+
+    description: 'Paul Jenkinson is an acoustic artist and accomplished songwriter who can be found often busking on Grafton Street.',
+    para1: 'Mainly found capturing ears and hearts while busking along Grafton Street, Paul Jenkinson is an acoustic artist who is also an accomplished songwriter.',
+    para2: 'Inspired by a wide range of talents, including Hozier, Biffy Clyro and The 1975, his music hints at these influences, while also featuring his own fresh sound. Jenkinson recorded his latest release with Jacob Koopman, of Phantom Records, a good friend and fellow busker.',
+    twitter: 'https://twitter.com/Paul_Jenkinson_?s=17',
+    insta: 'https://www.instagram.com/pauljenkomusic/',
+    facebook: 'https://www.facebook.com/pauljenkomusic/',
+    youtube: 'https://www.youtube.com/channel/UC1XkRRVbQSS8-3X48iMGOVA',
+  },
+
+{
+    name: 'Sam Clifford',
+
+    image: 'https://i.ytimg.com/vi/tO3NQfkNiaQ/maxresdefault.jpg',
+    videos: 'https://www.youtube.com/embed/videoseries?list=PL7Hzq1i4Oc9Bo8YyPLdXLBy15sF-Fn_Aq',
+    other: 'Interview',
+    live: 'https://www.youtube.com/embed/Wi7FI8DA5qU',
+    musict1: 'Channel Uploads',
+    musict2: 'Collaboration',
+    music1: 'https://www.youtube.com/embed/videoseries?list=UUBZk7aAZWC3-G7xWlVUp09Q',
+    music2: 'https://www.youtube.com/embed/x3iIbqy0PCM',
+
+    description: 'Aged 15, Saibh is from Dublin and busks on Grafton Street regularly, playing covers.',
+    para1: 'Aged 15, Saibh is from Dublin and busks on Grafton Street regularly, playing covers.',
+    para2: '',
+    twitter: 'https://twitter.com/Samcliffordmus1',
+    insta: 'https://www.instagram.com/samcliffordmusic/',
+    facebook: '',
+    youtube: 'https://www.youtube.com/channel/UCBZk7aAZWC3-G7xWlVUp09Q',
+  },
+
+  
+
+];
